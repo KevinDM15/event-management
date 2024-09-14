dcc1ca74b7cf66e4b1abd7a4d80ca839d7749a3d
kevindm14 <kevindiazm.14@hotmail.com>
2024-09-13 19:35:52 -0500

    fix: :art: remove logs

--- src/config/database/setupDB.ts
+++ src/config/database/setupDB.ts
@@ -4,8 +4,6 @@ import 'dotenv/config';
 export const setupDB = async (fastify: FastifyInstance) => {
   const pg = fastify.pg
 
-  console.log('pg', fastify.pg)
-
   // Check if the database exists
   const checkDB = await pg.query(`SELECT 1 FROM pg_database WHERE datname = '${process.env.DB_NAME}'`)
   if (checkDB.rowCount > 0) {
